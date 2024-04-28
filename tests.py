3a61e96111a1156625adef4aa31e200b477c1ba8
stalin2009 <vitek200967@mail.ru>
2024-04-28 19:53:15 +0300

    add tests

--- tests.py
+++ tests.py
@@ -1,24 +1,110 @@
-from main import BooksCollector
+import random
+import pytest
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_books_genre_fav_dict_is_empty(self, books_collector):
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(books_collector.books_genre) == 0 and len(books_collector.favorites) == 0
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_genre_list_is_not_empty(self, books_collector):
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        assert books_collector.genre == ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
+
+    def test_genre_age_rating_list_is_not_empty(self, books_collector):
+
+        assert books_collector.genre_age_rating == ['Ужасы', 'Детективы']
+
+    @pytest.mark.parametrize('book', ['Мстители', 'Пуп земли'])
+    def test_add_new_book_positive(self, books_collector, book):
+
+        books_collector.add_new_book(book)
+        assert books_collector.books_genre[book] == ''
+
+    @pytest.mark.parametrize('book', ['', 'Очень длинный фильм про мстителей и как они мстят друг другу и остальным'])
+    def test_add_new_book_more_negative_sizes(self, books_collector, book):
+
+        assert not books_collector.add_new_book(book)
+
+    def test_set_book_genre_valid_name(self, books_collector):
+
+        books_collector.add_new_book('Найти, кому мстить')
+        books_collector.set_book_genre('Найти, кому мстить', 'Детективы')
+        assert books_collector.books_genre['Найти, кому мстить'] == 'Детективы'
+
+    def test_get_book_genre_return_valid_name(self, books_collector):
+
+        books_collector.add_new_book('Симпсоны')
+        books_collector.set_book_genre('Симпсоны', 'Фантастика')
+        assert books_collector.get_book_genre('Симпсоны') == 'Фантастика'
+
+    def test_get_books_with_specific_genre_when_valid_genre(self, books_collector):
+
+        books_collector.add_new_book('Котопес')
+        books_collector.set_book_genre('Котопес', 'Мультфильмы')
+        assert books_collector.get_books_with_specific_genre('Мультфильмы') \
+               and type(books_collector.get_books_with_specific_genre('Мультфильмы')) == list
+
+
+    def test_get_books_genre_filled_dict(self, books_collector):
+
+        books = ['Котопес', 'Песокот', 'Мой брат Юлий', 'Первый под землей']
+        for name in books:
+            books_collector.add_new_book(name)
+
+        random_book = random.choice(books)
+        assert random_book in books_collector.get_books_genre() \
+            and type(books_collector.get_books_genre()) == dict
+
+    def test_get_books_genre_empty_dict(self, books_collector):
+
+        assert not books_collector.get_books_genre()
+
+    def test_get_books_for_children_correct_genre(self, books_collector):
+
+        books = ['Салат в кармане', 'Сиреневыый туман', 'Сильный и слабый', 'Сегодня ко второй', 'Уволили']
+        x = 0
+        for name in books:
+            books_collector.add_new_book(name)
+            books_collector.set_book_genre(name, books_collector.genre[x])
+            x += 1
+
+        for rating in books_collector.genre_age_rating:
+            assert rating not in books_collector.get_books_for_children()
+
+    def test_get_books_for_children_adult_rating(self, books_collector):
+
+        books = ['Дюймовочка', 'Снежная королева']
+        x = 0
+        for name in books:
+            books_collector.add_new_book(name)
+            books_collector.set_book_genre(name, books_collector.genre_age_rating[x])
+            x += 1
+
+        assert not books_collector.get_books_for_children()
+
+    def test_add_book_in_favorites_when_books_in_list(self, books_collector):
+
+        books_collector.add_new_book('Силомер')
+        books_collector.add_book_in_favorites('Силомер')
+
+        assert 'Силомер' in books_collector.favorites
+
+    def test_delete_book_from_favorites(self, books_collector):
+
+        books_collector.add_new_book('Узколобый Джо')
+        books_collector.add_book_in_favorites('Узколобый Джо')
+
+        books_collector.delete_book_from_favorites('Узколобый Джо')
+        assert 'Узколобый Джо' not in books_collector.favorites
+
+    def test_get_list_of_favorites_books_empty_list(self, books_collector):
+
+        books_collector.add_new_book('Сизый выпь')
+        books_collector.add_book_in_favorites('Сизый выпь')
+        books_collector.delete_book_from_favorites('Сизый выпь')
+
+        assert not books_collector.get_list_of_favorites_books()
+
+    
